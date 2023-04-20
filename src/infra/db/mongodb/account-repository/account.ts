228f09f4e68459dff313752de617990c9ea0898a
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-04-20 20:48:04 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,17 +3,19 @@ import { AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
+interface AccountModelMongoDB {
+  _id: string
+  name: string
+  password: string
+  email: string
+}
+
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountConnection = MongoHelper.getCollection('accounts')
-    await accountConnection.insertOne(accountData)
-    const account = {
-      id: 'any_valid',
-      name: 'any_name',
-      email: 'any_email@email.com',
-      password: 'any_password'
-    }
-
-    return account
+    const resultIdNewAccount = await accountConnection.insertOne(accountData)
+    const account = await accountConnection.findOne(resultIdNewAccount.insertedId) as unknown as AccountModel
+    const { _id, ...accountWithoutId } = account as unknown as AccountModelMongoDB
+    return Object.assign({}, accountWithoutId, { id: _id })
   }
 }
