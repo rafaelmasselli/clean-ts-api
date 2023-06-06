7496fe7ab25e7f54be8e32eabee50391e87a5c70
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-06-06 18:18:57 -0300

    test: ensure LoginController returns 500 if Authentication thorws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -111,4 +111,15 @@ describe('Login controller', () => {
     await sut.handle(makeFakeRequest())
     expect(authSpy).toHaveBeenCalledWith('any_email@email.com', 'any_password')
   })
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest
+      .spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
